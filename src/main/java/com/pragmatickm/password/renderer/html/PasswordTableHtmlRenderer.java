1967d48f65feff040030b00be42525504c66c25c
AO Industries, Inc <support@aoindustries.com>
2022-02-27 00:49:05 -0600

    Using final instead of abstract for static utility classes
    NetBeans 12.6 is warning "Constructor is never used" when abstract, and
    this cannot be suppressed with `@SuppressWarnings("unused")`.

--- src/main/java/com/pragmatickm/password/renderer/html/PasswordTableHtmlRenderer.java
+++ src/main/java/com/pragmatickm/password/renderer/html/PasswordTableHtmlRenderer.java
@@ -1,6 +1,6 @@
 /*
  * pragmatickm-password-renderer-html - Passwords rendered as HTML in a Servlet environment.
- * Copyright (C) 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021  AO Industries, Inc.
+ * Copyright (C) 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -59,7 +59,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public abstract class PasswordTableHtmlRenderer {
+public final class PasswordTableHtmlRenderer {
 
 	/** Make no instances. */
 	private PasswordTableHtmlRenderer() {throw new AssertionError();}
