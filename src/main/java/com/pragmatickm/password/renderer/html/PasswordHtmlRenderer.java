b561dc9c06d34f6e2fb949e827ebafb910451935
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:03:15 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/pragmatickm/password/renderer/html/PasswordHtmlRenderer.java
+++ src/main/java/com/pragmatickm/password/renderer/html/PasswordHtmlRenderer.java
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with pragmatickm-password-renderer-html.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.pragmatickm.password.renderer.html;
 
 import com.aoapps.html.any.AnyUnion_Palpable_Phrasing;
